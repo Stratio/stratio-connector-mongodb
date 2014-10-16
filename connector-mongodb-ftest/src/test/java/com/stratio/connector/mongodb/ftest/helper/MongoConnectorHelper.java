d938a0f2f25b1bbf60be56abbd44c9d77bf05e40
David <darroyo@stratio.com>
2014-10-16 19:10:00 +0200

    catched mongo validation

--- connector-mongodb-ftest/src/test/java/com/stratio/connector/mongodb/ftest/helper/MongoConnectorHelper.java
+++ connector-mongodb-ftest/src/test/java/com/stratio/connector/mongodb/ftest/helper/MongoConnectorHelper.java
@@ -1,20 +1,20 @@
 /*
-* Licensed to STRATIO (C) under one or more contributor license agreements.
-* See the NOTICE file distributed with this work for additional information
-* regarding copyright ownership. The STRATIO (C) licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership. The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.stratio.connector.mongodb.ftest.helper;
 
 import static com.stratio.connector.mongodb.core.configuration.ConfigurationOptions.HOST;
@@ -38,6 +38,7 @@ import com.stratio.connector.commons.ftest.helper.IConnectorHelper;
 import com.stratio.connector.mongodb.core.MongoConnector;
 import com.stratio.connector.mongodb.core.configuration.MongoClientConfiguration;
 import com.stratio.connector.mongodb.core.connection.MongoConnectionHandler;
+import com.stratio.connector.mongodb.core.exceptions.MongoValidationException;
 import com.stratio.meta.common.connector.ConnectorClusterConfig;
 import com.stratio.meta.common.connector.IConfiguration;
 import com.stratio.meta.common.connector.IConnector;
@@ -68,7 +69,11 @@ public class MongoConnectorHelper implements IConnectorHelper {
         this.clusterName = clusterName;
         MongoClientConfiguration clientConfig = new MongoClientConfiguration(getConnectorClusterConfig());
 
-        mongoClient = new MongoClient(clientConfig.getSeeds(), clientConfig.getMongoClientOptions());
+        try {
+            mongoClient = new MongoClient(clientConfig.getSeeds(), clientConfig.getMongoClientOptions());
+        } catch (MongoValidationException e) {
+            throw new InitializationException("validation error", e);
+        }
 
     }
 
