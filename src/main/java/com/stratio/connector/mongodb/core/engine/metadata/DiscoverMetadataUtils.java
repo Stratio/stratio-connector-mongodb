29ea7c575ab6d7066e1cf0d89498c7987ccb87f0
David <darroyo@stratio.com>
2015-01-02 14:44:10 +0100

    added unit test

--- src/main/java/com/stratio/connector/mongodb/core/engine/metadata/DiscoverMetadataUtils.java
+++ src/main/java/com/stratio/connector/mongodb/core/engine/metadata/DiscoverMetadataUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to STRATIO (C) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.  The STRATIO (C) licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.stratio.connector.mongodb.core.engine.metadata;
 
 import java.util.ArrayList;
@@ -11,6 +28,8 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.CommandResult;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
+import com.mongodb.MapReduceCommand;
+import com.mongodb.MapReduceCommand.OutputType;
 import com.stratio.connector.commons.metadata.IndexMetadataBuilder;
 import com.stratio.crossdata.common.metadata.IndexMetadata;
 import com.stratio.crossdata.common.metadata.IndexType;
@@ -22,15 +41,17 @@ public class DiscoverMetadataUtils {
     }
 
     public static List<String> discoverField(DBCollection collection) {
-        // TODO implement with mapReduceCommand
-        // MapReduceCommand mapReduceCommand = new MapReduceCommand(inputCollection, map, reduce, outputCollection,
-        // type, query)
+        String map = "function() { for (var field in this) { emit(field, null); }}";
+        String reduce = "function(field, stuff) { return null; }";
+        MapReduceCommand mapReduceCommand = new MapReduceCommand(collection, map, reduce, null, OutputType.INLINE, null);
+        DBObject getFieldsCommand = mapReduceCommand.toDBObject();
+        CommandResult command = collection.getDB().command(getFieldsCommand);
 
-        DBObject discoverFieldCommand = new BasicDBObject("mapreduce", collection.getName());
-        discoverFieldCommand.put("map", "function() { for (var field in this) { emit(field, null); }}");
-        discoverFieldCommand.put("reduce", "function(field, stuff) { return null; }");
-        discoverFieldCommand.put("out", new BasicDBObject("inline", 1));
-        CommandResult command = collection.getDB().command(discoverFieldCommand);
+        // DBObject discoverFieldCommand = new BasicDBObject("mapreduce", collection.getName());
+        // discoverFieldCommand.put("map", "function() { for (var field in this) { emit(field, null); }}");
+        // discoverFieldCommand.put("reduce", "function(field, stuff) { return null; }");
+        // discoverFieldCommand.put("out", new BasicDBObject("inline", 1));
+        // CommandResult command = collection.getDB().command(discoverFieldCommand);
         BasicDBList results = (BasicDBList) command.get("results");
         Set<String> fields = new HashSet<>();
         for (Object object : results) {
@@ -43,7 +64,7 @@ public class DiscoverMetadataUtils {
     public static List<IndexMetadata> discoverIndexes(DBCollection collection) {
         // TODO add TextIndex, Geospatial,etc...
         // TODO supported only simple, compound and hashed index
-        // TODO remove _id
+        // TODO remove _id?
         // TODO return options?? e.g sparse, unique??
         // TODO custom (asc and desc)
         List<DBObject> indexInfo = collection.getIndexInfo();
@@ -64,6 +85,13 @@ public class DiscoverMetadataUtils {
         return indexMetadataList;
     }
 
+    /**
+     * Gets the index type.
+     *
+     * @param key
+     *            the key
+     * @return DEFAULT when involved fields have ascending index. CUSTOM otherwise
+     */
     private static IndexType getIndexType(BasicDBObject key) {
         boolean isDefault = true;
         IndexType indexType;
@@ -71,7 +99,7 @@ public class DiscoverMetadataUtils {
         Iterator<Object> iterator = key.values().iterator();
 
         while (iterator.hasNext() && isDefault == true) {
-            isDefault = iterator.next().toString().equals("1");
+            isDefault = iterator.next().toString().startsWith("1");
         }
 
         if (isDefault) {
